25b5bc23669d18f8487a455fd0b453da934d9eaf
JJSeger <jjsbowhunter@yahoo.com>
2017-03-13 16:58:57 -0400

    removed some unneeded comments and some whitespace
    removed some unneeded comments and some whitespace

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -3,37 +3,36 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Main extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
-	{
+    public function index(){
         $this->login();
 	}
-
-
 
 	public function login(){
         $this->load->view('login');
     }
 
+    public function members(){
+        $this->load->view('members');
+    }
+
+
     public function login_validation(){
 
 	    $this->load->library('form_validation');
-	    $this->form_validation->set_rules('email','Email','required');
-        $this->form_validation->set_rules('password','Password','required|md5');
+
+	    $this->form_validation->set_rules('email','Email','required|trim|xss_clean');
+        $this->form_validation->set_rules('password','Password','required|md5|trim');
+
+
+        if ($this->form_validation->run()){
+            redirect('main/members');
+        } else {
+            $this->load->view('login');
+        }
+
+        echo $_POST['email'];
+        echo $_POST['password'];
+
 
     }
 
