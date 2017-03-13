850a6330e3f7799be34603341109d0074d9743ed
JJSeger <jjsbowhunter@yahoo.com>
2017-03-13 17:01:27 -0400

    created members.php
    created members.php

---
+++ application/views/members.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Wooden Nickel Members</title>
+
+    </head>
+    <body>
+
+    <div id="container">
+        <h1>Welcome to the Wooden Nickel Members Page!</h1>
+
+    </div>
+
+    </body>
+</html>
