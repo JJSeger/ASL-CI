87ea4e5d7e3eacd9ca104791a5df98369680cfb4
JJSeger <jjsbowhunter@yahoo.com>
2017-03-13 17:00:06 -0400

    removed some of the styling
    removed some of the styling

--- application/views/login.php
+++ application/views/login.php
@@ -8,27 +8,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 	<style type="text/css">
 
-	::selection { background-color: #E13300; color: white; }
-	::-moz-selection { background-color: #E13300; color: white; }
 
-	body {
-		background-color: #fff;
-		margin: 40px;
-		font: 13px/20px normal Helvetica, Arial, sans-serif;
-		color: #4F5155;
-	}
-
-
-
-	h1 {
-		color: #444;
-		background-color: transparent;
-		border-bottom: 1px solid #D0D0D0;
-		font-size: large;
-		font-weight: bold;
-
-		padding: 14px 15px 10px 15px;
-	}
     #container {
         font-size: larger;
         width:50%;
@@ -47,11 +27,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <?php
     echo form_open('main/login_validation');
 
-    echo "<p>Email:";
+    echo validation_errors();
+
+    echo "<p>Email: ";
     echo form_input('email');
     echo"</p>";
 
-    echo "<p>Password:";
+    echo "<p>Password: ";
     echo form_password('password');
     echo"</p>";
 
