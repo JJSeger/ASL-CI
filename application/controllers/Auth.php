3cfda6f0855eeddec4af384eebf3b3e065143346
JJSeger <jjsbowhunter@yahoo.com>
2017-03-13 22:47:32 -0400

    added registration function to load the registration view
    added registration function to load the registration view

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -7,5 +7,8 @@ class Auth extends CI_Controller{
     public function login(){
         echo 'Welcome to the Wooden Nickel Login Page';
     }
+    public function register(){
+        $this->load->view('register');
+    }
 
 }
