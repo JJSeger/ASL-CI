a76a50af6c2f6840e59f159aba8df97362c14cd8
JJSeger <jjsbowhunter@yahoo.com>
2017-03-18 22:17:32 -0400

    trying everything i can think of to get images to display with codeigniter and it is proving to be challenging for some reason.
    trying everything i can think of to get images to display with
    codeigniter and it is proving to be challenging for some reason.

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -6,6 +6,7 @@ class User extends CI_Controller
     public function __construct()
     {
         parent::__construct();
+      
         if ($_SESSION['user_logged'] == FALSE ) {
             $this->session->set_flashdata("error", "Please login first to view this page");
             header("Location:http://localhost:8888/ASL-CI/index.php/auth/login");
