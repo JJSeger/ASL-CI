5a56a2b1edb1535a87b106dd0cc146648d84979f
JJSeger <jjsbowhunter@yahoo.com>
2017-03-21 19:35:50 -0400

    added cart to autoload libraries and form to the helpers - changed the default_controller to cart from welcome and created the the cart.php file in the controllers directory and created the function cart
    added cart to autoload libraries and form to the helpers - changed the
    default_controller to cart from welcome and created the the cart.php
    file in the controllers directory and created the function cart

---
+++ application/controllers/cart.php
@@ -0,0 +1,13 @@
+<?php
+
+class Cart extends Controller { // Our Cart class extends the Controller class
+
+    function Cart()
+    {
+        parent::Controller(); // We define the the Controller class is the parent.
+    }
+
+
+}
+/* End of file cart.php */
+/* Location: ./application/controllers/cart.php */
