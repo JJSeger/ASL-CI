01d4296c9b6042df26b0b8d05f037f0fcac4daca
JJSeger <jjsbowhunter@yahoo.com>
2017-03-21 20:05:25 -0400

    adding print functionality to cart.php
    adding print functionality to cart.php

--- application/controllers/cart.php
+++ application/controllers/cart.php
@@ -11,8 +11,8 @@ class Cart extends Controller { // Our Cart class extends the Controller class
 }
 /* End of file cart.php */
 /* Location: ./application/controllers/cart.php */
-
 function index()
 {
     $data['products'] = $this->cart_model->retrieve_products(); // Retrieve an array with all products
+    print_r($data['products']); // Print out the array to see if it works (Remove this line when done testing)
 }
