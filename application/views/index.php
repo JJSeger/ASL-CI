ff0d821a5ffb8cd93fb6712f5a9c176d366efac9
JJSeger <jjsbowhunter@yahoo.com>
2017-03-21 20:07:32 -0400

    adding the core template for the shopping cart
    adding the core template for the shopping cart

--- application/views/index.php
+++ application/views/index.php
@@ -1,7 +1,41 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: JuddHome
- * Date: 3/21/17
- * Time: 7:55 PM
- */
+<!DOCTYPE html>
+<html xml:lang="en-us">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>CodeIgniter Shopping Cart</title>
+
+    <link href="<?php echo base_url(); ?>assets/css/core.css" media="screen" rel="stylesheet" type="text/css" />
+
+
+    <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/core.js"></script>
+</head>
+<body>
+
+<div id="wrap">
+
+    <?php $this->view($content); ?>
+
+</div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
