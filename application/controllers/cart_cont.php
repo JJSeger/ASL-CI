3857eb75ac7850f09e73688b4111fdfce20acb3d
JJSeger <jjsbowhunter@yahoo.com>
2017-03-23 20:49:32 -0400

    added some autoloads, and linked the cart_cont to the Product_model
    added some autoloads, and linked the cart_cont to the Product_model

--- application/controllers/cart_cont.php
+++ application/controllers/cart_cont.php
@@ -5,6 +5,10 @@ class cart_cont extends CI_Controller {
 
     public function cart()
     {
-        $this->load->view('cart');
+        //Get all products
+        $data['products'] = $this->Product_model->get_products();
+
+
+        $this->load->view('cart',$data);
     }
 }
