df2c28fba94481d87b771d49763d11b5beba1c77
JJSeger <jjsbowhunter@yahoo.com>
2017-03-23 21:38:24 -0400

    got rid of the Product_model
    got rid of the Product_model

--- application/controllers/cart_cont.php
+++ application/controllers/cart_cont.php
@@ -1,14 +1,12 @@
 <?php
 
 
-class cart_cont extends CI_Controller {
+class cart_cont extends CI_Controller
+{
 
     public function cart()
     {
-        //Get all products
-        $data['products'] = $this->Product_model->get_products();
 
-
-        $this->load->view('cart',$data);
+        $this->load->view('cart');
     }
 }
